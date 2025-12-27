b7c9fd369d2f80d16e6fe364b1c946bd22944af6
GP-z007-alt <gokulprasath05072007@gmail.com>
2025-12-28 03:02:10 +0530

    Code refactor [NORMAL]

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,2 +1,22 @@
-import app from './app.js'
-console.log(app)
+import app from './app.js';
+import dotenv from 'dotenv';
+dotenv.config({path:'BackEnd/config/config.env'})
+const port = process.env.PORT || 3000;
+
+const getAllProducts = (req,res) => {
+    res.status(200).json({
+        message : "All Products"
+    })
+}
+const getSingleProduct = (req,res) => {
+    res.status(200).json({
+        message : "Single Product"
+    })
+}
+
+app.route("/api/v1/products").get(getAllProducts)
+app.route("/api/v1/product").get(getSingleProduct)
+
+app.listen(port,()=>{
+    console.log(`Server is running on PORT ${port}`)
+})
