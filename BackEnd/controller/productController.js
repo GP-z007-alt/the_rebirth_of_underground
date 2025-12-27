f6bd6b5629f7bafb8cb7e8e5277a71d70f05b748
GP-z007-alt <gokulprasath05072007@gmail.com>
2025-12-28 03:45:38 +0530

    Products schema

--- BackEnd/controller/productController.js
+++ BackEnd/controller/productController.js
@@ -1,4 +1,10 @@
+import Product from '../models/productModel.js'
 
+// Creating Products
+export const createProducts = async(req,res)=>{
+    console.log(req.body)
+    // await Product.create(req.body)
+}
 export const getAllProducts = (req,res) => {
     res.status(200).json({
         message : "All Products"
---
+++ BackEnd/models/productModel.js
@@ -0,0 +1,70 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,"Please enter Product name"],
+        trim:true
+    },
+    description:{
+        type:String,
+        required:[true,"Please enter Product description"],
+    },
+    price:{
+        type:Number,
+        required:[true,"Please enter Product Price"],
+        maxLength:[7,"Price can't exceed 7 Digit!"]
+    },
+    ratings:{
+        type:Number,
+        default:0,
+    },
+    image:[
+        {
+            public_id:{
+                type:String,
+                required:true
+            },
+            url:{
+                type:String,
+                required:true
+            }
+        }
+    ],
+    category:{
+        type:String,
+        required:[true,"Please enter Product Category"],
+    },
+    stock:{
+        type:Number,
+        required:[true,"Please enter product stock"],
+        maxLength:[5,"Stock can't exceed 5 Digit!"],
+        default:1
+    },
+    numofReviews:{
+        type:Number,
+        default:0,
+    },
+    reviews:[
+        {
+            name:{
+                type:String,
+                required:true
+            },
+            ratings:{
+                type:Number,
+                required:true
+            },
+            comment:{
+                type:String,
+                required:true
+            }
+        }
+    ],
+    createdAt:{
+        type:Date,
+        default:Date.now
+    }
+})
+
+export default mongoose.model("Product" ,productSchema )
