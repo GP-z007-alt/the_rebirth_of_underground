32c47e1e30c4725b608f74159c1f970341c907b3
GP-z007-alt <gokulprasath05072007@gmail.com>
2025-12-28 03:24:49 +0530

    Connection made with DB

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,6 +1,8 @@
 import app from './app.js';
 import dotenv from 'dotenv';
+import { connectMongoDatabase } from './config/db.js';
 dotenv.config({path:'BackEnd/config/config.env'})
+connectMongoDatabase();
 const port = process.env.PORT || 3000;
 
 app.listen(port,()=>{
