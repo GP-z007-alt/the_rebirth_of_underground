c79175c2700490b96831b02902c6dead8fbc7e11
GP-z007-alt <gokulprasath05072007@gmail.com>
2025-12-28 04:03:05 +0530

    Product getting and updating-> start

--- BackEnd/controller/productController.js
+++ BackEnd/controller/productController.js
@@ -2,13 +2,32 @@ import Product from '../models/productModel.js'
 
 // Creating Products
 export const createProducts = async(req,res)=>{
-    console.log(req.body)
-    // await Product.create(req.body)
-}
-export const getAllProducts = (req,res) => {
-    res.status(200).json({
-        message : "All Products"
+    // console.log(req.body)
+    const product = await Product.create(req.body)
+    res.status(201).json({
+        message : "Product Created Successfully",
+        product
     })
 }
 
+// Getting All Products
+export const getAllProducts = async(req,res) => {
+    const products = await Product.find()
+    res.status(200).json({
+        message : "All Products",
+        products
+    })
+}
+// Update Product
+export const updateProduct = async(req,res) => {
+    const product = await Product.findById(req.params.id);
+    console.log(product);
+    if(!product){
+        return res.status(500).json({
+            sucess : false,
+            message : "Product Not Found"
+        })
+    }
+    
+}
 
