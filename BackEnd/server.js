6ad79ce8daaae74db9a5f02642a825a785da7347
GP-z007-alt <gokulprasath05072007@gmail.com>
2025-12-28 03:14:57 +0530

    Better structure

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -3,20 +3,6 @@ import dotenv from 'dotenv';
 dotenv.config({path:'BackEnd/config/config.env'})
 const port = process.env.PORT || 3000;
 
-const getAllProducts = (req,res) => {
-    res.status(200).json({
-        message : "All Products"
-    })
-}
-const getSingleProduct = (req,res) => {
-    res.status(200).json({
-        message : "Single Product"
-    })
-}
-
-app.route("/api/v1/products").get(getAllProducts)
-app.route("/api/v1/product").get(getSingleProduct)
-
 app.listen(port,()=>{
     console.log(`Server is running on PORT ${port}`)
 })
