834eac44cb0a185a9ea09a9034d09881cb67aafa
GP-z007-alt <gokulprasath05072007@gmail.com>
2025-12-29 21:11:19 +0530

    User Login and cookies

--- BackEnd/controller/userController.js
+++ BackEnd/controller/userController.js
@@ -1,5 +1,7 @@
 import handleAsyncError from '../Middleware/handleAsyncError.js';
 import User from "../models/userModel.js";
+import HandleError from '../Utils/HandleError.js';
+import { sendToken } from '../Utils/jwtToken.js';
 export const registerUser = handleAsyncError(async (req, res, next) => {
     const {name,email,password} = req.body;
     const user = await User.create({
@@ -11,10 +13,23 @@ export const registerUser = handleAsyncError(async (req, res, next) => {
             url:"profilepicUrl"
         }
     });
-    const token = user.getJWTToken();
-    res.status(201).json({
-        success:true,
-        user,
-        token
-    });
+    sendToken(user,201,res);
+});
+
+// Login
+export const loginUser = handleAsyncError(async (req, res, next) => {
+    const {email,password} = req.body;
+    if(!email || !password){
+        return next(new HandleError("Please enter email and password",400));
+    }
+    const user = await User.findOne({email}).select("+password");
+    if(!user){
+        return next(new HandleError("Invalid email or password",401));
+    }
+    const isPasswordValid = await user.verifyPassword(password);
+    if(!isPasswordValid){
+        return next(new HandleError("Invalid email or password",401));
+    }
+    sendToken(user,200,res);
+
 });
