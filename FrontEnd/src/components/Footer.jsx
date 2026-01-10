afeb987385d2632ad4e4eecf6ed4589e2b3bd2f4
GP-z007-alt <gokulprasath05072007@gmail.com>
2026-01-11 01:56:07 +0530

    Did some shit

--- FrontEnd/src/components/Footer.jsx
+++ FrontEnd/src/components/Footer.jsx
@@ -1,12 +1,44 @@
 import React from 'react'
 import '../componentStyles/Footer.css'
+import { Phone, Mail, GitHub, YouTube, Instagram } from '@mui/icons-material';
 
 function Footer() {
   return (
     <footer className="footer">
       <div className="footer-container">
         {/*Section 1*/}
-        
+        <div className='footer-section contact'>
+          <h3>Contact Us</h3>
+          <p><Phone fontSize='small'/>Phone : 1234567890</p>
+          <p><Mail fontSize='small'/>Email : info@undergroundmafia.com</p>
+        </div>
+
+        {/*Section 2*/}
+        <div className='footer-section social'>
+          <h3>Follow Us</h3>
+          <div className='social-links'>
+            <a href='' target='_blank'>
+              <GitHub className='social-icon'/>
+            </a>
+            <a href='' target='_blank'>
+              <YouTube className='social-icon'/>
+            </a>
+            <a href='' target='_blank'>
+              <Instagram className='social-icon'/>
+            </a>
+          </div>
+        </div>
+
+        {/*Section 3*/}
+        <div className="footer-section about">
+          <h3>About Underground Mafia</h3>
+          <p>
+            Underground Mafia is a community dedicated to bringing together enthusiasts from all walks of life. Our mission is to foster connections, share knowledge, and create unforgettable experiences.
+          </p>
+        </div>
+      </div>
+      <div className='footer-container'>
+        <p>&copy; 2026 Underground Mafia. All rights reserved.</p>
       </div>
     </footer>
   )
