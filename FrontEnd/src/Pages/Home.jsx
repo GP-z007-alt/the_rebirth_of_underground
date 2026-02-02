e3d8693a02e79fd60807c484c2e68cc582cacdbe
GP-z007-alt <gokulprasath05072007@gmail.com>
2026-02-03 00:15:21 +0530

    LOMG

--- FrontEnd/src/Pages/Home.jsx
+++ FrontEnd/src/Pages/Home.jsx
@@ -3,10 +3,12 @@ import Footer from '../components/Footer'
 import '../pageStyles/Home.css'
 import NavBar from '../components/NavBar'
 import ImageSlider from '../components/ImageSlider'
-import Product from '../components/product'
+import Product from '../components/Product'
 import PageTitle from '../components/PageTitle'
 import { useDispatch, useSelector } from 'react-redux'
-import { getProduct } from '../features/products/productSlice'
+import { getProduct, removeErrors } from '../features/products/productSlice'
+import Loader from '../components/Loader'
+import { toast } from 'react-toastify'
 
 
 
@@ -16,8 +18,15 @@ const Home = () => {
   useEffect(() => {
     dispatch(getProduct());
   },[dispatch])
+  useEffect(() => {
+    if(error){
+      toast.error(error.message,{position:'top-center',autoClose:3000});
+      dispatch(removeErrors());
+    }
+  },[dispatch,error])
   return (
     <>
+    {loading ? (<Loader />) : (<>
     <PageTitle title="Home"/>
     <NavBar/>
     <ImageSlider/>
@@ -30,8 +39,9 @@ const Home = () => {
       </div>
     </div>
     <Footer/> 
+    </>)}
     </>
-  )
+  );
 }
 
 export default Home
